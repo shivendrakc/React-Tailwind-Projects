32c33f79df2192cfe88cdf619c095e8819b33094
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2025-03-21 10:44:47 +1100

    This is my new commit for my first React/Tailwind Project. I am already halfway through and thought might as well commit everyday

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
+import Price from './components/Price'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <WorkFlow />
+        <Price />
+      </div>
+    </>
+  )
+}
+
+export default App
