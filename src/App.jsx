1a9d794e74b10581b79998745e1aa4b138463b73
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2025-03-21 22:37:28 +1100

    I completed this Project Completely Now will be added to one of my showcase project. More to come

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
 import WorkFlow from './components/WorkFlow'
 import Price from './components/Price'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <FeatureSection />
         <WorkFlow />
         <Price />
+        <Testimonials />
+        <Footer />
       </div>
     </>
   )
